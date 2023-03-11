19cc57d324be75eca19be2a08916687ee1b30e05
Jeff Campbell <86787426+citizenjett@users.noreply.github.com>
2023-03-10 19:13:32 -0500

    add library svc book return and heros

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import './App.css';
 import { Carousel } from './layouts/HomePage/Carousel';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Heros } from './layouts/HomePage/Heros';
+import { LibraryServices } from './layouts/HomePage/LibraryServices';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Navbar />
       <ExploreTopBooks />
       <Carousel />
+      <Heros />
+      <LibraryServices />
     </div>
   );
 }
